0b615b0ac156085439726a09ed441589ed18d0b5
uma6 <>
2018-09-07 23:59:15 +0900

    Add main class and function.

---
+++ src/main/java/com/companyname/apps/TestApi.java
@@ -0,0 +1,11 @@
+package com.companyname.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApi {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApi.class, args);
+    }
+}
