37c1a340b0375a1e622bb2cb9d45829fa39954d8
uma6 <>
2018-11-23 11:06:05 +0900

    外部のTomcat上にデプロイできるようにするためのコードを追加(デプロイエラーになる)

--- src/main/java/com/companyname/apps/TestApi.java
+++ src/main/java/com/companyname/apps/TestApi.java
@@ -2,11 +2,23 @@ package com.companyname.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// SpringBootServletInitializerの継承はwarファイルを作って別のtomcatにデプロイするために必要
+//  参考 => https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file
 @SpringBootApplication
-public class TestApi {
+public class TestApi extends SpringBootServletInitializer {
+
+    // warファイルを作って別のtomcatにデプロイするために必要
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TestApi.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TestApi.class, args);
     }
 
 }
+
